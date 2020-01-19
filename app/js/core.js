a77b2cdec2e621f54dc585d0b3b2c14324b37da1
rommelmamedov <rommelmamedov@gmail.com>
2020-01-19 16:07:17 +0400

    blog-article page done

--- app/js/core.js
+++ app/js/core.js
@@ -10,5 +10,11 @@
       'color: #A97BFF; font-weight: 300; font-size: 28px;',
       'color: #A97BFF; font-weight: 800; font-size: 28px;'
     );
+    // $('.blog .articles').masonry({
+    //   // options
+    //   itemSelector: '.article-card',
+    //   columnWidth: 340,
+    //   gutter: 20
+    // });
   });
 })();
